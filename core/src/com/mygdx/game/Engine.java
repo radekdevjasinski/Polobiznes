9a791fa1ec2b0477f7fd303986f4e7e49ed191cc
AnchorDev <01.jakub.kotwica@gmail.com>
2023-10-18 01:56:04 +0200

    test v0.1

--- core/src/com/mygdx/game/Engine.java
+++ core/src/com/mygdx/game/Engine.java
@@ -19,7 +19,7 @@ public class Engine extends ApplicationAdapter {
 	public void render () {
 		ScreenUtils.clear(1, 0, 0, 1);
 		batch.begin();
-		batch.draw(img, 100, 0);
+		batch.draw(img, 175, 150);
 		batch.end();
 	}
 	
