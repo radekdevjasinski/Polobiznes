b0fe00b1047d9deb73562b2d768f3440000bc5f0
Elizaa0 <elizajanus41@gmail.com>
2023-11-08 13:25:41 +0100

    Kartypoczatek

--- core/src/com/mygdx/game/Card.java
+++ core/src/com/mygdx/game/Card.java
@@ -11,8 +11,8 @@ public class Card {
     int costWithTheHouse3;
     int costWithTheHouse4;
     int costWithTheHotel;
-
-    public Card(String nameCity, int costOfPurchase, int costPerCottage, int costForHotel, int parkingCost, int costWithTheHouse1, int costWithTheHouse2, int costWithTheHouse3, int costWithTheHouse4, int costWithTheHotel) {
+    int  mortgage;
+    public Card(String nameCity, int costOfPurchase, int costPerCottage, int costForHotel, int parkingCost, int costWithTheHouse1, int costWithTheHouse2, int costWithTheHouse3, int costWithTheHouse4, int costWithTheHotel, int mortgage) {
         this.nameCity = nameCity;
         this.costOfPurchase = costOfPurchase;
         this.costPerCottage = costPerCottage;
@@ -23,5 +23,95 @@ public class Card {
         this.costWithTheHouse3 = costWithTheHouse3;
         this.costWithTheHouse4 = costWithTheHouse4;
         this.costWithTheHotel = costWithTheHotel;
+        this.mortgage = mortgage;
     }
+
+    public String getNameCity() {
+        return nameCity;
+    }
+
+    public void setNameCity(String nameCity) {
+        this.nameCity = nameCity;
+    }
+
+    public int getCostOfPurchase() {
+        return costOfPurchase;
+    }
+
+    public void setCostOfPurchase(int costOfPurchase) {
+        this.costOfPurchase = costOfPurchase;
+    }
+
+    public int getCostPerCottage() {
+        return costPerCottage;
+    }
+
+    public void setCostPerCottage(int costPerCottage) {
+        this.costPerCottage = costPerCottage;
+    }
+
+    public int getCostForHotel() {
+        return costForHotel;
+    }
+
+    public void setCostForHotel(int costForHotel) {
+        this.costForHotel = costForHotel;
+    }
+
+    public int getParkingCost() {
+        return parkingCost;
+    }
+
+    public void setParkingCost(int parkingCost) {
+        this.parkingCost = parkingCost;
+    }
+
+    public int getCostWithTheHouse1() {
+        return costWithTheHouse1;
+    }
+
+    public void setCostWithTheHouse1(int costWithTheHouse1) {
+        this.costWithTheHouse1 = costWithTheHouse1;
+    }
+
+    public int getCostWithTheHouse2() {
+        return costWithTheHouse2;
+    }
+
+    public void setCostWithTheHouse2(int costWithTheHouse2) {
+        this.costWithTheHouse2 = costWithTheHouse2;
+    }
+
+    public int getCostWithTheHouse3() {
+        return costWithTheHouse3;
+    }
+
+    public void setCostWithTheHouse3(int costWithTheHouse3) {
+        this.costWithTheHouse3 = costWithTheHouse3;
+    }
+
+    public int getCostWithTheHouse4() {
+        return costWithTheHouse4;
+    }
+
+    public void setCostWithTheHouse4(int costWithTheHouse4) {
+        this.costWithTheHouse4 = costWithTheHouse4;
+    }
+
+    public int getCostWithTheHotel() {
+        return costWithTheHotel;
+    }
+
+    public void setCostWithTheHotel(int costWithTheHotel) {
+        this.costWithTheHotel = costWithTheHotel;
+    }
+
+    public int getMortgage() {
+        return mortgage;
+    }
+
+    public void setMortgage(int mortgage) {
+        this.mortgage = mortgage;
+    }
+
 }
--- core/src/com/mygdx/game/Game.java
+++ core/src/com/mygdx/game/Game.java
@@ -3,14 +3,17 @@ package com.mygdx.game;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Game {
     public static void main(String[] args) {
         String filePath = "assets/kartyMiast.txt";
         try {
-            String fileContent = readFile(filePath);;
+            String fileContent = readFile(filePath);
+            ;
             System.out.println(fileContent);
-            System.out.println("Zawartość"+filePath);
+            System.out.println("Zawartość" + filePath);
         } catch (IOException e) {
             System.err.println("Wystąpił błąd podczas odczytu pliku: " + e.getMessage());
         }
@@ -25,5 +28,48 @@ public class Game {
             }
         }
         return content.toString();
+    }
+
+    public void listCard() {
+        List<Card> listCard = new ArrayList<>();
+        String filePath = "assets/kartyMiast.txt";
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] parts = line.split(",");
+                if (parts.length == 11) {
+                    String nameCity = parts[0];
+                    int costOfPurchase = Integer.parseInt(parts[1]);
+                    int costPerCottage = Integer.parseInt(parts[2]);
+                    int costForHotel = Integer.parseInt(parts[3]);
+                    int parkingCost = Integer.parseInt(parts[4]);
+                    int costWithTheHouse1 = Integer.parseInt(parts[5]);
+                    int costWithTheHouse2 = Integer.parseInt(parts[6]);
+                    int costWithTheHouse3 = Integer.parseInt(parts[7]);
+                    int costWithTheHouse4 = Integer.parseInt(parts[8]);
+                    int costWithTheHotel = Integer.parseInt(parts[9]);
+                    int  mortgage = Integer.parseInt(parts[10]);
+                    Card card = new Card("Miasto", 120, 100, 100, 5, 40, 60, 180, 320, 500, 60
+                    );
+                    card.setNameCity(nameCity);
+                    card.setCostOfPurchase(costOfPurchase);
+                    card.setCostPerCottage(costPerCottage);
+                    card.setCostForHotel(costForHotel);
+                    card.setParkingCost(parkingCost);
+                    card.setCostWithTheHouse1(costWithTheHouse1);
+                    card.setCostWithTheHouse2(costWithTheHouse2);
+                    card.setCostWithTheHouse3(costWithTheHouse3);
+                    card.setCostWithTheHouse4(costWithTheHouse4);
+                    card.setCostWithTheHotel(costWithTheHotel);
+                    card.setMortgage(mortgage);
+                    listCard.add(card);
+                } else {
+                    System.out.println("Nieprawidłowy format danych: " + line);
+                }
+
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
